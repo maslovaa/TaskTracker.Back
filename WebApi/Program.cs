73517e67cc904b03e55df380439373d847eee3d4
Dima <belousdima777@yandex.ru>
2024-05-10 23:58:04 +0300

    Добавил класс контекста DataContext и настроил его в program.cs.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,7 +1,13 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
