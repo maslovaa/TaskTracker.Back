55878eea973209c591a077c4c4cc0f4fc8932cd5
Dima <belousdima777@yandex.ru>
2024-05-13 01:07:07 +0300

    Add TaskEntity

---
+++ Domain/Entities/TaskEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class TaskEntity : IdEntity
+    {
+        public string Ticket { get; set; }
+        public string Head { get; set; }
+        public string Body { get; set; }
+        public string Comment { get; set; }
+    }
+}
