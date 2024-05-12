601e238def61a75584d0a5c64d53764405af35da
Dima <belousdima777@yandex.ru>
2024-05-12 23:47:32 +0300

    Add DeskDto.

---
+++ Models/DTO/DeskDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTO
+{
+    public class DeskDto
+    {
+        /// <summary>
+        /// Идентификатор доски
+        /// </summary>
+        public Guid? Id { get; set; }
+
+        /// <summary>
+        /// Наименование доски
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
