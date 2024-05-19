1de2385065eeb86ad04efeb34c274c1b51ce99e0
Konstantin Porutchikov <porutchikov@gmail.com>
2024-05-19 21:50:12 +0300

    TRACKER-25

--- WebApi/Controllers/RolesController.cs
+++ WebApi/Controllers/RolesController.cs
@@ -1,5 +1,7 @@
-﻿using DataAccess.Repositories;
+﻿using AutoMapper;
+using DataAccess.Repositories;
 using Domain.Abstractions;
+using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Models.Dto;
 
@@ -7,41 +9,41 @@ namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class RolesController(IRolesRepository _rolesRepository) : ControllerBase
+    public class RolesController(IRolesRepository _rolesRepository, IMapper _mapper) : ControllerBase
     {
         // GET: api/Roles
         [HttpGet]
         public async Task<ActionResult<IEnumerable<RoleDto>>> GetAsync()
         {
-            return await _rolesRepository.GetRolesByPredicateAsync(x => true);
+            return _mapper.Map<List<RoleDto>>(await _rolesRepository.GetByPredicate(x => true, CancellationToken.None));
         }
 
         // GET api/Roles/<Guid>
         [HttpGet("{id}")]
         public async Task<ActionResult<RoleDto>> GetAsync(Guid id)
         {
-            return await _rolesRepository.FindRoleByIdAsync(id);
+            return _mapper.Map<RoleDto>(await _rolesRepository.GetByIdAsync(id, CancellationToken.None));
         }
 
         // POST api/Roles
         [HttpPost]
         public async Task<ActionResult<Guid>> Post([FromBody] RoleDto roleDto)
         {
-            return await _rolesRepository.AddRoleAsync(roleDto);
+            return await _rolesRepository.AddAsync(_mapper.Map<RoleEntity>(roleDto));
         }
 
         // PUT api/Roles
         [HttpPut]
         public async Task<ActionResult<bool>> Put([FromBody] RoleDto roleDto)
         {
-            return await _rolesRepository.UpdateRoleAsync(roleDto);
+            return await _rolesRepository.Update(_mapper.Map<RoleEntity>(roleDto));
         }
 
         // DELETE api/Roles/<Giud>
         [HttpDelete("{id}")]
         public async Task<ActionResult<bool>> Delete(Guid id)
         {
-            return await _rolesRepository.DeleteRoleAsync(id);
+            return await _rolesRepository.Delete(id, CancellationToken.None);
         }
     }
 }
