b3eb180315489782f2a5e3ce07b606c11a85ed50
Nikolay Kovalenko <web.update@mail.ru>
2024-05-28 19:53:18 +0300

    [TRACKER-30] Фикс теста на маппинг UserEntity.

--- Tests/Services/AutoMapper/UserMapperTests.cs
+++ Tests/Services/AutoMapper/UserMapperTests.cs
@@ -22,7 +22,7 @@ public class UserMapperTests
     }
 
     [Test]
-    public void Map_WhenGivenValidSourceObject_ReturnsValidOriginObject()
+    public void Map_WhenGivenValidSourceObjectUserDtoOrUserEntity_ReturnsValidOriginObjectUserDtoOrUserEntity()
     {
         // Arrange
         
@@ -31,6 +31,7 @@ public class UserMapperTests
             UserName = "SomeUserName",
             Email = "someMailBox@domain.ru"
         };
+
         var userEntity = UserBuilder.CreateBaseUser();
         
         // Act
@@ -40,6 +41,40 @@ public class UserMapperTests
         // Assert
         Assert.AreEqual(userDto.UserName, userResult.UserName);
         Assert.AreEqual(userDto.Email, userResult.Email);
+        Assert.AreEqual(userEntity.UserName, userDtoResult.UserName);
+        Assert.AreEqual(userEntity.Email, userDtoResult.Email);
+        
+    }
+    
+    [Test]
+    public void Map_WhenGivenValidSourceObjectCreatingUserDtoOrUserEntity_ReturnsValidOriginObjectCreatingUserDtoOrUserEntity()
+    {
+        // Arrange
+
+        var creatingUserDto = new CreatingUserDto()
+        {
+            Name = "SomeNewUserName",
+            Surname = "SomeNewUserSurname",
+            Patronymic = "SomeNewUserPatronymic",
+            UserName = "SomeNewUserNickname",
+            Email = "SomeNewUserEmail"
+        };
+
+        var userEntity = UserBuilder.CreateBaseUser();
+        
+        // Act
+        var userResult = _mapper.Map<UserEntity>(creatingUserDto);
+        var userDtoResult = _mapper.Map<CreatingUserDto>(userEntity);
+        
+        // Assert
+        Assert.AreEqual(creatingUserDto.Name, userResult.Name);
+        Assert.AreEqual(creatingUserDto.Surname, userResult.Surname);
+        Assert.AreEqual(creatingUserDto?.Patronymic, userResult?.Patronymic);
+        Assert.AreEqual(creatingUserDto.UserName, userResult.UserName);
+        Assert.AreEqual(creatingUserDto.Email, userResult.Email);
+        Assert.AreEqual(userEntity.Name, userDtoResult.Name);
+        Assert.AreEqual(userEntity.Surname, userDtoResult.Surname);
+        Assert.AreEqual(userEntity?.Patronymic, userDtoResult?.Patronymic);
         Assert.AreEqual(userEntity.UserName, userDtoResult.UserName);
         Assert.AreEqual(userEntity.Email, userDtoResult.Email);
     }
