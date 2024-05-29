36e93e2451f2c74887ef558a167c47473a9bfe55
Dima <belousdima777@yandex.ru>
2024-05-29 19:26:09 +0300

    Удалил GetAll.

--- Domain/Abstractions/IRepository.cs
+++ Domain/Abstractions/IRepository.cs
@@ -12,13 +12,6 @@ public interface IRepository<T, TId>
     where T : IEntity<TId>
 {
     /// <summary>
-    /// Получение всех сущностей.
-    /// </summary>
-    /// <param name="noTracking">Флаг отслеживания.</param>
-    /// <returns>Объект запроса.</returns>
-    IQueryable<T> GetAll(bool noTracking = false);
-
-    /// <summary>
     /// Добавление сущностей в хранилище.
     /// </summary>
     /// <param name="entity">Сущность.</param>
