8668199168f716deb1d9706908b251ca5e59f254
maslovaa <almas-site@yandex.ru>
2024-05-30 22:51:59 +0300

    TRACKER-38

--- Domain/Entities/DeskEntity.cs
+++ Domain/Entities/DeskEntity.cs
@@ -5,6 +5,7 @@ namespace Domain.Entities
     public class DeskEntity : NamedEntity, IIsActive
     {
         public ProjectEntity Project { get; set; }
+        public string Descripption { get; set; }
         public IEnumerable<TaskEntity> Tasks { get; set; }
         public bool IsActive { get; set; }
     }
