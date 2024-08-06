4ac8eb1dd881c4b345311b867fcedf0169e62b83
Алексей Маслов <maslov@anc.space>
2024-08-06 23:03:53 +0300

    TRACKER-52 Добавление методов в контроллеры Statuses и Tasks

--- WebApi/Controllers/StatusesController.cs
+++ WebApi/Controllers/StatusesController.cs
@@ -2,6 +2,7 @@
 using DataAccess.Repositories;
 using Domain.Abstractions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Models.Dto;
 using Models.DTO;
 
@@ -16,6 +17,12 @@ namespace WebApi.Controllers
         public async Task<ActionResult<StatusDto>> GetAsync(Guid id)
         {
             return _mapper.Map<StatusDto>(await _statusesRepository.GetByIdAsync(id, CancellationToken.None));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<StatusDto>>> GetAsync()
+        {
+            return _mapper.Map<List<StatusDto>>(await _statusesRepository.GetAll().ToListAsync());
         }
     }
 }
