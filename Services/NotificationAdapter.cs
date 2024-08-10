917fae0f80c85a7897b0df583f6a92076d15ede6
Dima <belousdima777@yandex.ru>
2024-08-10 04:24:10 +0300

    TRACKER-53: Сделал Adapter для нотификации.

---
+++ Services/NotificationAdapter.cs
@@ -0,0 +1,27 @@
+﻿using Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class NotificationAdapter : INotificationAdapter
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationAdapter(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public void ProcessSend<T>(T model)
+        {
+            var json = JsonSerializer.Serialize(model);
+
+            _notificationService.Send(json);
+        }
+    }
+}
---
+++ Services/NotificationService.cs
@@ -0,0 +1,22 @@
+﻿using Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class NotificationService : INotificationService
+    {
+        public void Send(string message)
+        {
+            // здесь будет брокер.
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(message);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
