89f8ac10b22c9a5b7ef65d3a1285d7e13241d9d0
Nikolay Kovalenko <web.update@mail.ru>
2024-08-20 23:47:11 +0300

    [TRACKER-55] Добавление сервиса и контроллера для взаимодействия с сервером идентификации.

---
+++ Models/DTO/IdentityDtos/IdentityResponse/IdentityResponse.cs
@@ -0,0 +1,15 @@
+﻿using Models.Dto;
+using System;
+using System.Text.Json.Serialization;
+
+namespace Models.DTO.IdentityDtos.IdentityResponse
+{
+    public class IdentityResponse
+    {
+        [JsonPropertyName("token")]
+        public string Token { get; set; }
+
+        [JsonPropertyName("errorMessage")]
+        public string ErrorMessage { get; set; }
+    }
+}
